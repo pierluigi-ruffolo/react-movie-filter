aa25988ba716d5a7ed549aade124b24e85f85a8f
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-02 14:58:39 +0100

    feat: aggiungo la funzionalità filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const ArrayFilm = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+
 function App() {
   const [valueSelect, SetValueSelect] = useState("");
+
+  const [filmArray, SetFilmArray] = useState(ArrayFilm);
+
+  useEffect(() => {
+    if (valueSelect === "") {
+      SetFilmArray(ArrayFilm);
+    } else {
+      const newArrayFilter = ArrayFilm.filter((element) => {
+        return element.genre === valueSelect;
+      });
+      SetFilmArray(newArrayFilter);
+    }
+  }, [valueSelect]);
 
   return (
     <>
@@ -24,6 +46,16 @@ function App() {
           <option value="Romantico">Romantico</option>
           <option value="Azione">Azione</option>
         </select>
+        {filmArray.map((film, index) => {
+          return (
+            <div className="card mt-5" key={index}>
+              <div className="card-body">
+                <h5 className="card-title fs-3">Genere: {film.genre}</h5>
+                <h6 className="card-text fs-4">Film: {film.title}</h6>
+              </div>
+            </div>
+          );
+        })}
       </main>
     </>
   );
